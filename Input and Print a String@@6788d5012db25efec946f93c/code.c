65f6fb51e450a17f2e532497f06bc1ca69374b10
MayankG17957 <goyal.mayank1706@gmail.com>
2025-01-28 18:18:30 +0530

    Solution to Input and Print a String

---
+++ Input and Print a String@@6788d5012db25efec946f93c/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char string;
+    scanf("%c",&string);
+    printf("You entered: &c");
+    return 0;
+}
